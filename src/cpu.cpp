e63081b3214b716fb79dc40928a0f22ea6d06708
Jochem Arends <joch.arends@gmail.com>
2024-01-19 14:58:09 +0100

    Commiting before changing a lot.

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -11,14 +11,27 @@ namespace lc3 {
     void cpu::execute(std::uint16_t bin) {
         auto opcode = bin >> 12;
         switch (opcode) {
-        case add::opcode: {
-            auto [a, b, c] = decode<add>(bin);
+        case opcode::add {
+            std::uint16_t a = (bin >> 9) & 0x05;
+            std::uint16_t b = (bin >> 6) & 0x05;
+            std::uint16_t c = bin & 0x1F;
+            auto [a, b, c] = encoding<opcode::add, reg, reg, bits<0, 0, 0>, reg>::decode(bin);
 
-            if (bit_at<5>(bin) == 0) {
-                c = regs[c];
+            if ((bin >> 5) & 1)  {
+                return encoding<:add, reg, reg, bits<0, 0, 0>, reg>::decode(bin);
+            } 
+            else {
+                return encoding<opcode::add, reg, reg, bits<1>, imm5>::decode(bin);
             }
 
-            regs[a] = regs[b] + c;
+            /* ADD */
+            auto [a, b, c] = add::decode(bin);
+            
+            if (!(bin >> 5) & 1) {
+                c = m_regs[c];
+            }
+
+            m_regs[a] = m_regs[b] + c;
         }
         }
     }
